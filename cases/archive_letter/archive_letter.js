f3828eadbf9dfea11f1fedef5ace75333b5ebcf7
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-16 16:00:36 +0300

    trying to fix delete()

--- cases/archive_letter/archive_letter.js
+++ cases/archive_letter/archive_letter.js
@@ -1,17 +1,14 @@
 import main from '../../steps/main';
-import layout from '../../steps/layout'
-import folders from '../../steps/sidebar/folders';
 import letters from '../../steps/letters';
 import buttons from '../../steps/portal/button';
 import returnToInbox from '../../steps';
 
-// пример теста
 describe('archive test', () => {
 	it('Авторизоваться и открыть первое письмо на странице и архивировать его', () => {
 		main.open('https://mail.ru');
 		main.login(process.env.LOGIN, process.env.PASSWORD);
 		
-		letters.openBySubject("Вход с нового устройства"); // выбор письма по теме
+		letters.openBySubject("Вход с нового устройства");
 		buttons.clickByName('В архив');
 
 		returnToInbox('archive');		
--- cases/delete_letter/delete_letter.js
+++ cases/delete_letter/delete_letter.js
@@ -1,18 +1,16 @@
 import main from '../../steps/main';
-import layout from '../../steps/layout'
-import folders from '../../steps/sidebar/folders';
 import letters from '../../steps/letters';
 import buttons from '../../steps/portal/button';
-import returnToInbox from '../../steps/utils';
+import returnToInbox from '../../steps/index';
 
 describe('delete test', () => {
 	it('Авторизоваться и открыть первое письмо на странице и проверить наличие кнопки удалить и удалить это письмо', () => {
 		main.open('https://mail.ru');
 		main.login(process.env.LOGIN, process.env.PASSWORD);
-		
-		letters.openByLetterNumber(1);
+
+		letters.openBySubject("Вход с нового устройства");
 		buttons.clickByName('Удалить');
 
-		returnToInbox('trash')
+		returnToInbox('trash');
 	});
 });
