cb057d112ad340f5c1987a541a34c51247a1af39
nikitarub <nikita.rub@yandex.ru>
2019-05-21 09:04:26 +0300

    Fixed reply test

--- cases/replyForward_letter/replyForward_letter.js
+++ cases/replyForward_letter/replyForward_letter.js
@@ -8,16 +8,6 @@ describe('reply and forward test', () => {
         main.open('https://mail.ru');
         main.login(process.env.LOGIN, process.env.PASSWORD);
     });
-    it('Авторизоваться и открыть первое письмо на странице. Ответить на письмо.', () => {
-
-        let subject = 'test1';
-
-        letters.openBySubject(subject);
-        buttons.clickByName('Ответить');
-
-        letters.checkReplyForwardSubject(subject, true); // проверка темы сообщения, в данном случае на ответ
-        letters.closeReplyForwardWindow();
-    });
     it('Авторизоваться и открыть первое письмо на странице. Переслать на письмо.', () => {
         main.open('https://octavius.mail.ru/inbox/');
 
@@ -28,5 +18,17 @@ describe('reply and forward test', () => {
 
         letters.checkReplyForwardSubject(subject, false); // проверка темы сообщения, в данном случае на ответ
         letters.closeReplyForwardWindow();
+        
+    });
+    it('Авторизоваться и открыть первое письмо на странице. Ответить на письмо.', () => {
+
+        let subject = 'test1';
+        main.open('https://octavius.mail.ru/inbox/');
+        letters.openBySubject(subject);
+        buttons.clickByName('Ответить');
+
+        letters.checkReplyForwardSubject(subject, true); // проверка темы сообщения, в данном случае на ответ
+        letters.closeReplyForwardWindow();
+        // main.open('https://octavius.mail.ru/inbox/');
     });
 });
